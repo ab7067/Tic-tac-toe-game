dfb4a69ad94d66bc7ef0b26e8a8196cd08fe3937
ab7067 <ab7067@srmist.edu.in>
2026-04-21 11:21:39 +0530

    coded to console

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,21 +1,20 @@
 package org.example;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
+        int slot = 7;
 
-        int slot = getUserSlot();
-        System.out.println("Slot entered: " + slot);
+        System.out.println("Row: " + getRowFromSlot(slot));
+        System.out.println("Column: " + getColFromSlot(slot));
     }
 
-    // Reads slot input from user
-    static int getUserSlot() {
-        Scanner sc = new Scanner(System.in);
+    // Convert slot (1–9) → row (0–2)
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
 
-        System.out.print("Enter a slot number (1-9): ");
-        int slot = sc.nextInt();
-
-        return slot;
+    // Convert slot (1–9) → column (0–2)
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
     }
 }
