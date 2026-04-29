30eeb8d2970a568ac4595da739ec9e14b6775fa5
ab7067 <ab7067@srmist.edu.in>
2026-04-29 10:49:36 +0530

    coded to console

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,16 +1,35 @@
-static void computerMove() {
-    Random rand = new Random();
+static boolean hasWon(char symbol) {
 
-    int row, col;
-
-    while (true) {
-        row = rand.nextInt(3); // 0–2
-        col = rand.nextInt(3); // 0–2
-
-        if (board[row][col] == '-') {
-            board[row][col] = computerSymbol;
-            System.out.println("Computer placed at: " + row + ", " + col);
-            break;
+    // Check rows
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == symbol &&
+            board[i][1] == symbol &&
+            board[i][2] == symbol) {
+            return true;
         }
     }
+
+    // Check columns
+    for (int j = 0; j < 3; j++) {
+        if (board[0][j] == symbol &&
+            board[1][j] == symbol &&
+            board[2][j] == symbol) {
+            return true;
+        }
+    }
+
+    // Check diagonals
+    if (board[0][0] == symbol &&
+        board[1][1] == symbol &&
+        board[2][2] == symbol) {
+        return true;
+    }
+
+    if (board[0][2] == symbol &&
+        board[1][1] == symbol &&
+        board[2][0] == symbol) {
+        return true;
+    }
+
+    return false;
 }
