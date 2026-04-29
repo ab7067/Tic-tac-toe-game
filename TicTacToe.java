da6e9a0b6ee604fd4c83d0279ba206f6f25a4f04
ab7067 <ab7067@srmist.edu.in>
2026-04-29 10:58:40 +0530

    coded to console

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,35 +1,25 @@
-static boolean hasWon(char symbol) {
+public class TicTacToe {
 
-    // Check rows
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == symbol &&
-            board[i][1] == symbol &&
-            board[i][2] == symbol) {
-            return true;
+    static boolean isHumanTurn = true;
+    static boolean gameOver = false;
+
+    public static void main(String[] args) {
+
+        while (!gameOver) {
+
+            if (isHumanTurn) {
+                System.out.println("Human's turn");
+                // call method to take input + place move
+            } else {
+                System.out.println("Computer's turn");
+                // call method for computer move
+            }
+
+            // Switch turn
+            isHumanTurn = !isHumanTurn;
+
+            // TEMP condition to stop loop (remove later)
+            gameOver = true;
         }
     }
-
-    // Check columns
-    for (int j = 0; j < 3; j++) {
-        if (board[0][j] == symbol &&
-            board[1][j] == symbol &&
-            board[2][j] == symbol) {
-            return true;
-        }
-    }
-
-    // Check diagonals
-    if (board[0][0] == symbol &&
-        board[1][1] == symbol &&
-        board[2][2] == symbol) {
-        return true;
-    }
-
-    if (board[0][2] == symbol &&
-        board[1][1] == symbol &&
-        board[2][0] == symbol) {
-        return true;
-    }
-
-    return false;
 }
