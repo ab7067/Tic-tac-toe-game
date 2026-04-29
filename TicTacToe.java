8cb169353b2e04f6076d2336702e1c40368bec6b
ab7067 <ab7067@srmist.edu.in>
2026-04-29 10:43:02 +0530

    coded to console

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,44 +1,13 @@
-package org.example;
-public class Main {
+public class TicTacToe {
 
     static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
-
-        initializeBoard();
-
-        int row = 1;
-        int col = 1;
-
-        if (isValidMove(row, col)) {
-            System.out.println("Move is valid.");
-        } else {
-            System.out.println("Invalid move.");
-        }
+        placeMove(0, 0, 'X');
+        System.out.println(board[0][0]);
     }
 
-    // Initialize board with '-'
-    static void initializeBoard() {
-        for (int r = 0; r < 3; r++) {
-            for (int c = 0; c < 3; c++) {
-                board[r][c] = '-';
-            }
-        }
-    }
-
-    // Validate move
-    static boolean isValidMove(int row, int col) {
-
-        // Check bounds
-        if (row < 0 || row > 2 || col < 0 || col > 2) {
-            return false;
-        }
-
-        // Check if cell is empty
-        if (board[row][col] != '-') {
-            return false;
-        }
-
-        return true;
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
     }
 }
